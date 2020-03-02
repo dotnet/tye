b09e4755e8a352ac8113e89a9d8a051b29008e6d
Ryan Nowak <nowakra@gmail.com>
2020-03-01 16:12:45 -0800

    Add frontend/backend sample (works with tye run)

---
+++ samples/frontend-backend/frontend/ConfigurationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static Uri? GetServiceUri(this IConfiguration configuration, string name)
+        {
+            var host = configuration[$"service:{name}:host"];
+            var port = configuration[$"service:{name}:port"];
+            var protocol = configuration[$"service:{name}:protocol"] ?? "http";
+
+            if (string.IsNullOrEmpty(host) || port == null)
+            {
+                return null;
+            }
+
+            return new Uri(protocol + "://" + host + ":" + port + "/");
+        }
+    }
+}
