388cbfb94e0defee3db069714b9b91c44a82b866
Justin Kotalik <jukotali@microsoft.com>
2020-03-03 18:11:03 -0800

    Update tests

---
+++ test/E2ETest/TyeRunTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Tye.ConfigModel;
+using Tye.Hosting;
+using Xunit;
+
+namespace E2ETest
+{
+    public class TyeRunTest
+    {
+        [Fact]
+        public async Task BasicE2ETest()
+        {
+            var application = ConfigFactory.FromFile();
+            TyeHost.RunAsync();
+        }
+    }
+}
