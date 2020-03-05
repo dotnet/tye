080038d8d4fe1a37e395b7b36cba76f482e72970
Justin Kotalik <jukotali@microsoft.com>
2020-03-05 12:27:41 -0800

    Add license and headers

--- src/Tye.Core/DeployServiceYamlStep.cs
+++ src/Tye.Core/DeployServiceYamlStep.cs
@@ -1,9 +1,12 @@
-﻿using System.CommandLine.Invocation;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.CommandLine.Invocation;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tye;
 using YamlDotNet.RepresentationModel;
 
 namespace Tye
