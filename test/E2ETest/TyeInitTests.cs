49799726e546284f0219455ffe29e75a56b5aebd
Justin Kotalik <jukotali@microsoft.com>
2020-03-05 23:31:15 -0800

    Add tye init tests

--- test/E2ETest/TyeInitTests.cs
+++ test/E2ETest/TyeInitTests.cs
@@ -20,32 +20,46 @@ namespace E2ETest
 
             var projectFile = new FileInfo(Path.Combine(tempDirectory.DirectoryPath, "test-project.csproj"));
 
-            var (content, outptuFilePath) = InitHost.CreateTyeFileContent(projectFile, force: false);
-            //Assert.Equal();
-        }
+            var (content, _) = InitHost.CreateTyeFileContent(projectFile, force: false);
+            var expectedContent = File.ReadAllText("testassets/init/single-project.yaml");
 
-        [Fact]
-        public void FrontendBackendInitTest()
-        {
-            var projectDirectory = new DirectoryInfo(Path.Combine(TestHelpers.GetSolutionRootDirectory("tye"), "samples", "single-project", "test-project"));
-            using var tempDirectory = TempDirectory.Create();
-            DirectoryCopy.Copy(projectDirectory.FullName, tempDirectory.DirectoryPath);
-
-            var projectFile = new FileInfo(Path.Combine(tempDirectory.DirectoryPath, "test-project.csproj"));
-
-            var outputFilePath = InitHost.CreateTyeFile(projectFile, force: false);
+            Assert.Equal(expectedContent, content);
         }
 
         [Fact]
         public void MultiProjectInitTest()
         {
-            var projectDirectory = new DirectoryInfo(Path.Combine(TestHelpers.GetSolutionRootDirectory("tye"), "samples", "single-project", "test-project"));
+            var projectDirectory = new DirectoryInfo(Path.Combine(TestHelpers.GetSolutionRootDirectory("tye"), "samples", "multi-project"));
             using var tempDirectory = TempDirectory.Create();
             DirectoryCopy.Copy(projectDirectory.FullName, tempDirectory.DirectoryPath);
 
-            var projectFile = new FileInfo(Path.Combine(tempDirectory.DirectoryPath, "test-project.csproj"));
+            // delete already present yaml
+            File.Delete(Path.Combine(tempDirectory.DirectoryPath, "tye.yaml"));
 
-            var outputFilePath = InitHost.CreateTyeFile(projectFile, force: false);
+            var projectFile = new FileInfo(Path.Combine(tempDirectory.DirectoryPath, "multi-project.sln"));
+
+            var (content, _) = InitHost.CreateTyeFileContent(projectFile, force: false);
+            var expectedContent = File.ReadAllText("testassets/init/multi-project.yaml");
+
+            Assert.Equal(expectedContent, content);
+        }
+
+        [Fact]
+        public void FrontendBackendTest()
+        {
+            var projectDirectory = new DirectoryInfo(Path.Combine(TestHelpers.GetSolutionRootDirectory("tye"), "samples", "frontend-backend"));
+            using var tempDirectory = TempDirectory.Create();
+            DirectoryCopy.Copy(projectDirectory.FullName, tempDirectory.DirectoryPath);
+
+            // delete already present yaml
+            File.Delete(Path.Combine(tempDirectory.DirectoryPath, "tye.yaml"));
+
+            var projectFile = new FileInfo(Path.Combine(tempDirectory.DirectoryPath, "frontend-backend.sln"));
+
+            var (content, _) = InitHost.CreateTyeFileContent(projectFile, force: false);
+            var expectedContent = File.ReadAllText("testassets/init/frontend-backend.yaml");
+
+            Assert.Equal(expectedContent, content);
         }
     }
 }
