0d05542e3f37265f5df62fc0eff5e33e226251a0
Justin Kotalik <jkotalik12@gmail.com>
2020-03-20 10:51:48 -0700

    Create new json objects for service models (#154)

---
+++ src/Microsoft.Tye.Hosting/Model/V1/V1ReplicaType.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.Tye.Hosting.Model.V1
+{
+    public enum V1ReplicaType
+    {
+        Process,
+        Docker
+    }
+}
---
+++ src/Microsoft.Tye.Hosting/Model/V1/V1Service.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Tye.Hosting.Model.V1
+{
+    public class V1Service
+    {
+        public V1ServiceDescription? Description { get; set; }
+        public ServiceType ServiceType { get; set; }
+        public int Restarts { get; set; }
+        public V1ServiceStatus? Status { get; set; }
+        public Dictionary<string, V1ReplicaStatus>? Replicas { get; set; }
+    }
+}
