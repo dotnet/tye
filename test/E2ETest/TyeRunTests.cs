33ef331b074998f4e6a4b22012868c0915f3c9b6
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-04-02 10:09:55 -0400

    Fix whitespaces

--- test/E2ETest/TyeRunTests.cs
+++ test/E2ETest/TyeRunTests.cs
@@ -307,10 +307,10 @@ namespace E2ETest
             application.Network = dockerNetwork;
 
             var handler = new HttpClientHandler
-            {
-                ServerCertificateCustomValidationCallback = (a, b, c, d) => true,
-                AllowAutoRedirect = false
-            };
+                          {
+                              ServerCertificateCustomValidationCallback = (a, b, c, d) => true,
+                              AllowAutoRedirect = false
+                          };
 
             var client = new HttpClient(new RetryHandler(handler));
 
