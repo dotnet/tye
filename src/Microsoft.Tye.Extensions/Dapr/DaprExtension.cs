a61c4a87ae44b56845106d6510e70724ab3797c2
thangchung <thangchung@ymail.com>
2020-04-16 12:58:19 +0700

    add default value to overcome nullable possibly

--- src/Microsoft.Tye.Extensions/Dapr/DaprExtension.cs
+++ src/Microsoft.Tye.Extensions/Dapr/DaprExtension.cs
@@ -138,17 +138,15 @@ namespace Microsoft.Tye.Extensions.Dapr
                         deployment.Annotations.Add("dapr.io/id", project.Name);
                         deployment.Annotations.Add("dapr.io/port", (httpBinding.Port ?? 80).ToString(CultureInfo.InvariantCulture));
 
-#nullable disable
                         if (config.Data.TryGetValue("config", out var daprConfig) && daprConfig is object)
                         {
-                            deployment.Annotations.TryAdd("dapr.io/config", daprConfig!.ToString());
+                            deployment.Annotations.TryAdd("dapr.io/config", daprConfig!.ToString() ?? string.Empty);
                         }
 
                         if (config.Data.TryGetValue("log-level",  out var logLevel) && logLevel is object)
                         {
-                            deployment.Annotations.TryAdd("dapr.io/log-level", logLevel!.ToString());
+                            deployment.Annotations.TryAdd("dapr.io/log-level", logLevel!.ToString() ?? string.Empty);
                         }
-#nullable enable
                     }
                 }
             }
