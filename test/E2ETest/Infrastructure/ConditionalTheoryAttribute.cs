b61c8a8de307826eaeb85edb89b6f7fe0c128bf1
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-04-20 15:52:52 -0400

    Add tests to verify the output directory for the corresponding build configuration

---
+++ test/E2ETest/Infrastructure/ConditionalTheoryAttribute.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace E2ETest
+{
+    using System;
+
+    using Xunit;
+    using Xunit.Sdk;
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    [XunitTestCaseDiscoverer("E2ETest." + nameof(ConditionalTheoryDiscoverer), "Microsoft.Tye.E2ETest")]
+    public class ConditionalTheoryAttribute : TheoryAttribute
+    {
+    }
+}
---
+++ test/E2ETest/Infrastructure/ConditionalTheoryDiscoverer.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace E2ETest
+{
+    using Xunit.Abstractions;
+    using Xunit.Sdk;
+
+    internal class ConditionalTheoryDiscoverer : TheoryDiscoverer
+    {
+        private readonly IMessageSink _diagnosticMessageSink;
+
+        public ConditionalTheoryDiscoverer(IMessageSink diagnosticMessageSink)
+            : base(diagnosticMessageSink)
+        {
+            _diagnosticMessageSink = diagnosticMessageSink;
+        }
+
+        protected override IXunitTestCase CreateTestCaseForTheory(
+            ITestFrameworkDiscoveryOptions discoveryOptions,
+            ITestMethod testMethod,
+            IAttributeInfo theoryAttribute)
+        {
+            var skipReason = testMethod.EvaluateSkipConditions();
+
+            return skipReason != null
+                       ? new SkippedTestCase(skipReason, _diagnosticMessageSink, discoveryOptions.MethodDisplayOrDefault(), TestMethodDisplayOptions.None, testMethod)
+                       : base.CreateTestCaseForTheory(discoveryOptions, testMethod, theoryAttribute);
+        }
+    }
+}
