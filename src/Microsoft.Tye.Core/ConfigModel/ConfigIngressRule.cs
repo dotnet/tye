84c43a034148a488f3094a4b7dc645c76e51020d
Ryan Nowak <nowakra@gmail.com>
2020-04-22 17:18:35 -0700

    Treat IngressRuleBuilder.Service as non-null

--- src/Microsoft.Tye.Core/ConfigModel/ConfigIngressRule.cs
+++ src/Microsoft.Tye.Core/ConfigModel/ConfigIngressRule.cs
@@ -2,12 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Microsoft.Tye.ConfigModel
 {
     public class ConfigIngressRule
     {
         public string? Path { get; set; }
         public string? Host { get; set; }
+
+        [Required]
         public string? Service { get; set; }
     }
 }
