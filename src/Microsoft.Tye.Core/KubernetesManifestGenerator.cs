e2a281a4beb9ae1962fefc1c53768d3ce40fe723
David Fowler <davidfowl@gmail.com>
2020-04-29 00:59:56 -0700

    Fix slash for ingress deployment - Treat the / the same as empty string for ingress deployments

--- src/Microsoft.Tye.Core/KubernetesManifestGenerator.cs
+++ src/Microsoft.Tye.Core/KubernetesManifestGenerator.cs
@@ -88,7 +88,7 @@ namespace Microsoft.Tye
                         //
                         // Therefore our rewrite-target is set to $2 - we want to make sure we have
                         // two capture groups.
-                        if (string.IsNullOrEmpty(ingressRule.Path))
+                        if (string.IsNullOrEmpty(ingressRule.Path) || ingressRule.Path == "/")
                         {
                             path.Add("path", "/()(.*)"); // () is an empty capture group.
                         }
