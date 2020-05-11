621d950a9fac58a145a34c8a84ccb5323ddde557
Justin Kotalik <jukotali@microsoft.com>
2020-05-11 16:15:32 -0700

    Update Program.cs

--- test/E2ETest/testassets/projects/multi-project/worker/Program.cs
+++ test/E2ETest/testassets/projects/multi-project/worker/Program.cs
@@ -5,7 +5,6 @@
 using System;
 using System.IO;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.KeyPerFile;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
