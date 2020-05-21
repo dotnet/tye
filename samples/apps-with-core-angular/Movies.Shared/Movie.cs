1a428ab066f6314e55fb0feda6795282bf24184c
rahul sahay <rahulsahay19@hotmail.com>
2020-05-21 12:54:35 +0530

    angular dotnet core sample for project tye (#477)
    * angular dotnet core sample for project tye
    
    * Removed redundant code
    
    * Implemented comments

---
+++ samples/apps-with-core-angular/Movies.Shared/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movies.Shared
+{
+   public class Movie
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public string DirectorName { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
---
+++ samples/apps-with-core-angular/MoviesApp/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://localhost:5001/api/'
+};
