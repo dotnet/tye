8dbb2a56d17268c8c79af16a09cd9d979d13e350
TeBeCo <tebeco@gmail.com>
2020-06-26 23:27:23 +0200

    adding a sample project with plural form of TargetFrameworks for debugging purpose

---
+++ samples/app-with-targetframeworks/MultipleTargetFrameworks/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MultipleTargetFrameworks
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+#if NETCOREAPP3_1
+            app.Run(async context => await context.Response.WriteAsync("NETCOREAPP3_1"));
+#else
+            app.Run(async context => await context.Response.WriteAsync("NETCOREAPP2_2"));
+#endif
+        }
+    }
+}
