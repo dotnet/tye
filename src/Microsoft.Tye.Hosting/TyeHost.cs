be553a6288034533fa36c2246bab492bb7891251
Tom Deseyn <tom.deseyn@gmail.com>
2020-07-08 14:06:18 +0200

    cleanup

--- src/Microsoft.Tye.Hosting/TyeHost.cs
+++ src/Microsoft.Tye.Hosting/TyeHost.cs
@@ -326,11 +326,20 @@ namespace Microsoft.Tye.Hosting
         {
             try
             {
-                System.Diagnostics.Process.Start(new ProcessStartInfo
+                // https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    FileName = url,
-                    UseShellExecute = true
-                });
+                    url = url.Replace("&", "^&");
+                    System.Diagnostics.Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    System.Diagnostics.Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    System.Diagnostics.Process.Start("open", url);
+                }
             }
             catch (Exception ex)
             {
