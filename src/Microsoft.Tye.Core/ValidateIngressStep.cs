43e38c0d1225423bb5e1acf6a4cdb65103249b0f
John Luo <johluo@microsoft.com>
2020-07-16 10:38:57 -0700

    Add logging for KubectlDetector logic (#586)

--- src/Microsoft.Tye.Core/ValidateIngressStep.cs
+++ src/Microsoft.Tye.Core/ValidateIngressStep.cs
@@ -43,12 +43,12 @@ namespace Microsoft.Tye
                 return;
             }
 
-            if (!await KubectlDetector.Instance.IsKubectlInstalled.Value)
+            if (!await KubectlDetector.IsKubectlInstalledAsync(output))
             {
                 throw new CommandException($"Cannot validate ingress because kubectl is not installed.");
             }
 
-            if (!await KubectlDetector.Instance.IsKubectlConnectedToCluster.Value)
+            if (!await KubectlDetector.IsKubectlConnectedToClusterAsync(output))
             {
                 throw new CommandException($"Cannot validate ingress because kubectl is not connected to a cluster.");
             }
