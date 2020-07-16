4ca8e162474dff306fcde28fb03794d3eb6e53b4
John Luo <johluo@microsoft.com>
2020-07-15 20:17:40 -0700

    Add logging for KubectlDetector logic

--- src/shared/KubectlDetector.cs
+++ src/shared/KubectlDetector.cs
@@ -9,46 +9,48 @@ namespace Microsoft.Tye
 {
     internal class KubectlDetector
     {
-        public static KubectlDetector Instance { get; } = new KubectlDetector();
+        private static bool? _kubectlInstalled = null;
+        private static bool? _kubectlConnectedToCluster = null;
 
-        private KubectlDetector()
+        public static async Task<bool> IsKubectlInstalledAsync(OutputContext output)
         {
-            IsKubectlInstalled = new Lazy<Task<bool>>(DetectKubectlInstalled);
-            IsKubectlConnectedToCluster = new Lazy<Task<bool>>(DetectKubectlConnectedToCluster);
+            if (!_kubectlInstalled.HasValue)
+            {
+                output.WriteInfoLine("Verifying kubectl installation...");
+                try
+                {
+                    // Ignoring the exit code and relying on Process to throw if kubectl is not found
+                    // kubectl version will return non-zero if you're not connected to a cluster.
+                    await ProcessUtil.RunAsync("kubectl", "version", throwOnError: false);
+                    _kubectlInstalled = true;
+                }
+                catch (Exception)
+                {
+                    // Unfortunately, process throws
+                    _kubectlInstalled = false;
+                }
+            }
+
+            return _kubectlInstalled.Value;
         }
 
-        public Lazy<Task<bool>> IsKubectlInstalled { get; }
-
-        public Lazy<Task<bool>> IsKubectlConnectedToCluster { get; }
-
-        private static async Task<bool> DetectKubectlInstalled()
+        public static async Task<bool> IsKubectlConnectedToClusterAsync(OutputContext output)
         {
-            try
+            if (!_kubectlConnectedToCluster.HasValue)
             {
-                // Ignoring the exit code and relying on Process to throw if kubectl is not found
-                // kubectl version will return non-zero if you're not connected to a cluster.
-                await ProcessUtil.RunAsync("kubectl", "version", throwOnError: false);
-                return true;
+                output.WriteInfoLine("Verifying kubectl connection to cluster...");
+                try
+                {
+                    var result = await ProcessUtil.RunAsync("kubectl", "cluster-info", throwOnError: false);
+                    _kubectlConnectedToCluster = result.ExitCode == 0;
+                }
+                catch (Exception)
+                {
+                    // Unfortunately, process throws
+                    _kubectlConnectedToCluster = false;
+                }
             }
-            catch (Exception)
-            {
-                // Unfortunately, process throws
-                return false;
-            }
-        }
-
-        private static async Task<bool> DetectKubectlConnectedToCluster()
-        {
-            try
-            {
-                var result = await ProcessUtil.RunAsync("kubectl", "cluster-info", throwOnError: false);
-                return result.ExitCode == 0;
-            }
-            catch (Exception)
-            {
-                // Unfortunately, process throws
-                return false;
-            }
+            return _kubectlConnectedToCluster.Value;
         }
     }
 }
