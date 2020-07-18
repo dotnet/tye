9a9e9aaa1a34501281327346eac94cc5953bf7e7
TeBeCo <tebeco@gmail.com>
2020-07-18 22:18:12 +0200

    Add test and testasset project for both TargetFrameworks and TargetFramework

--- test/E2ETest/TyeRunTests.cs
+++ test/E2ETest/TyeRunTests.cs
@@ -902,7 +902,7 @@ services:
 
         [ConditionalFact]
         [SkipIfDockerNotRunning]
-        public async Task RunExplicitYamlBackendMultipleTargetFrameworkTest()
+        public async Task RunExplicitYamlMultipleTargetFrameworksTest()
         {
             using var projectDirectory = CopyTestProjectDirectory("multi-targetframeworks");
 
@@ -933,9 +933,40 @@ services:
 
         [ConditionalFact]
         [SkipIfDockerNotRunning]
-        public async Task RunBackendMultipleTargetFrameworkTest()
+        public async Task RunWithArgsMultipleTargetFrameworksTest()
         {
             using var projectDirectory = CopyTestProjectDirectory("multi-targetframeworks");
+
+            var projectFile = new FileInfo(Path.Combine(projectDirectory.DirectoryPath, "tye-no-buildproperties.yaml"));
+            var outputContext = new OutputContext(_sink, Verbosity.Debug);
+            var application = await ApplicationFactory.CreateAsync(outputContext, projectFile, "netcoreapp3.1");
+
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (a, b, c, d) => true,
+                AllowAutoRedirect = false
+            };
+
+            var client = new HttpClient(new RetryHandler(handler));
+
+            await RunHostingApplication(application, new HostOptions(), async (app, uri) =>
+            {
+                // make sure it is running
+                var backendUri = await GetServiceUrl(client, uri, "backend");
+
+                var backendResponse = await client.GetAsync(backendUri);
+                Assert.True(backendResponse.IsSuccessStatusCode);
+
+                var responseContent = await backendResponse.Content.ReadAsStringAsync();
+                Assert.Contains(".NET Core 3.1", responseContent);
+            });
+        }
+
+        [ConditionalFact]
+        [SkipIfDockerNotRunning]
+        public async Task RunIgnorePluralTargetFrameworksWhenSingularFormExistsTest()
+        {
+            using var projectDirectory = CopyTestProjectDirectory("multi-targetframeworks-and-targetframework-too");
 
             var projectFile = new FileInfo(Path.Combine(projectDirectory.DirectoryPath, "tye-no-buildproperties.yaml"));
             var outputContext = new OutputContext(_sink, Verbosity.Debug);
