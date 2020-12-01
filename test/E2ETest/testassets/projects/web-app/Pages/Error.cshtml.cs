af84b2d3e8f7dde268c49eb1ab284dcf04c23bbc
Hossam Barakat <Hossam.eldeen@gmail.com>
2020-12-02 04:31:08 +1100

    fix #666, enable dotnet watch to refresh on razer file changes (#831)
    * fix #666, enable dotnet watch to refresh on razer file changes
    
    * Fix code format

---
+++ test/E2ETest/testassets/projects/web-app/Pages/Error.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace web_app.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class ErrorModel : PageModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
