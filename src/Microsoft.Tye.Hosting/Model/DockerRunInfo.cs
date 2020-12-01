cbddac2388e2da37f352b7d8fbe6d4c5485d8894
Justin Kotalik <jukotali@microsoft.com>
2020-12-01 09:20:14 -0800

    Update DockerRunInfo.cs

--- src/Microsoft.Tye.Hosting/Model/DockerRunInfo.cs
+++ src/Microsoft.Tye.Hosting/Model/DockerRunInfo.cs
@@ -15,8 +15,11 @@ namespace Microsoft.Tye.Hosting.Model
             Image = image;
             Args = args;
         }
+        
         public bool IsProxy { get; set; }
+        
         public bool Private { get; set; }
+        
         public bool IsAspNet { get; set; }
 
         public string? NetworkAlias { get; set; }
@@ -26,6 +29,7 @@ namespace Microsoft.Tye.Hosting.Model
         public List<DockerVolume> VolumeMappings { get; } = new List<DockerVolume>();
 
         public string? Args { get; }
+        
         public Dictionary<string, string> BuildArgs { get; set; } = new Dictionary<string, string>();
 
         public string Image { get; }
