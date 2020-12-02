ff72bb8612ce1a807f9972050349df76a7370e84
sirh3e <sirh3e@protonmail.com>
2020-12-03 00:03:40 +0100

    refactored using added language feature

--- src/Microsoft.Tye.Hosting/Infrastructure/ProxyExtensions.cs
+++ src/Microsoft.Tye.Hosting/Infrastructure/ProxyExtensions.cs
@@ -201,10 +201,8 @@ namespace Microsoft.AspNetCore.Proxy
             // SendAsync removes chunking from the response. This removes the header so it doesn't expect a chunked response.
             response.Headers.Remove("transfer-encoding");
 
-            await using (var responseStream = await responseMessage.Content.ReadAsStreamAsync())
-            {
-                await responseStream.CopyToAsync(response.Body, StreamCopyBufferSize, context.RequestAborted);
-            }
+            await using var responseStream = await responseMessage.Content.ReadAsStreamAsync();
+            await responseStream.CopyToAsync(response.Body, StreamCopyBufferSize, context.RequestAborted);
         }
     }
 }
