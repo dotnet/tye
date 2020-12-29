e351bf658d52b2cf69c71c6c97a3b3cc11a3fe9d
Justin Kotalik <jukotali@microsoft.com>
2020-12-29 10:35:45 -0800

    Update IngressHostMatcherPolicy.cs

--- src/Microsoft.Tye.Hosting/Infrastructure/IngressHostMatcherPolicy.cs
+++ src/Microsoft.Tye.Hosting/Infrastructure/IngressHostMatcherPolicy.cs
@@ -136,11 +136,13 @@ namespace Microsoft.AspNetCore.Routing.Matching
             }
 
             if (hostParts.Length != 2)
+            {
                 throw new InvalidOperationException($"Could not parse host: {host}");
-
+            }
             if (string.IsNullOrEmpty(hostParts[0]))
+            {
                 throw new InvalidOperationException($"Could not parse host: {host}");
-
+            }
             if (int.TryParse(hostParts[1], out var port))
             {
                 return new EdgeKey(hostParts[0], port);
