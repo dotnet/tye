97af5e9997b4337edc585805688dc868be6c620c
Phillip Hoff <phillip@orst.edu>
2021-01-08 14:55:20 -0800

    More format warnings resolved?

--- src/Microsoft.Tye.Hosting/MdnsApplicationAdvertiser.cs
+++ src/Microsoft.Tye.Hosting/MdnsApplicationAdvertiser.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -18,7 +18,8 @@ namespace Microsoft.Tye.Hosting
 
             var discovery = new ServiceDiscovery();
 
-            try {
+            try
+            {
                 var profile = new ServiceProfile($"tye-{dashboardUri.Port}", "_microsoft-tye._tcp", (ushort)dashboardUri.Port);
 
                 discovery.Advertise(profile);
