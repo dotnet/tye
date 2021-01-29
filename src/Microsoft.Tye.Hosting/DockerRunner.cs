6d13fa95c7328267c384e804414aed39826900f3
Tim Potze <timpotze@gmail.com>
2021-01-29 21:22:43 +0100

    Update src/Microsoft.Tye.Hosting/DockerRunner.cs
    Co-authored-by: Justin Kotalik <jukotali@microsoft.com>

--- src/Microsoft.Tye.Hosting/DockerRunner.cs
+++ src/Microsoft.Tye.Hosting/DockerRunner.cs
@@ -240,7 +240,7 @@ namespace Microsoft.Tye.Hosting
                     // These are the ports that the application should use for binding
 
                     // 1. Tell the docker container what port to bind to
-                    portString = docker.Private ? "" : string.Join(" ", ports.Select(p => $"-p {p.Port}:{p.ContainerPort ?? p.Port}{(p.Protocol == "udp" ? "/udp" : string.Empty)}"));
+                    portString = docker.Private ? "" : string.Join(" ", ports.Select(p => $"-p {p.Port}:{p.ContainerPort ?? p.Port}{(string.Equals(p.Protocol, "udp", StringComparison.OrdinalIgnoreCase) ? "/udp" : string.Empty)}"));
 
                     if (docker.IsAspNet)
                     {
