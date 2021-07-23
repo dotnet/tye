d30f42728875271201961b8bda8b1c00b9524c1b
Phillip Hoff <phillip@orst.edu>
2021-07-23 10:21:57 -0700

    Give bindings default names.

--- src/Microsoft.Tye.Core/BindingBuilder.cs
+++ src/Microsoft.Tye.Core/BindingBuilder.cs
@@ -2,12 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 namespace Microsoft.Tye
 {
     public sealed class 
     BindingBuilder
     {
-        public string? Name { get; set; }
+        public string? Name { get; set; } = Guid.NewGuid().ToString();
         public string? ConnectionString { get; set; }
         public int? Port { get; set; }
         public int? ContainerPort { get; set; }
